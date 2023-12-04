62f928bf2445b164b7cf414d3e68c03b0fb4f25c
Mackie Burgess <mackie.burgess@gmail.com>
2023-12-04 08:23:56 +0000

    day 4 - part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,41 +1,70 @@
 use std::fs;
 use std::collections::HashSet;
 
-fn winnings() -> usize {
+fn wins(card: &str) -> usize {
+    if let Some((_, data)) = card.split_once(": ") {
+        if let Some((winning, ours)) = data.split_once(" | ") {
+            let winning = winning
+                .split_whitespace()
+                .filter_map(|v| v.parse::<usize>().ok())
+                .collect::<HashSet<usize>>();
+
+            let wins = ours
+                .split_whitespace()
+                .filter_map(|v| v.parse::<usize>().ok())
+                .filter(|v| winning.contains(v))
+                .collect::<Vec<usize>>()
+                .len();
+
+            return wins;
+        }
+    }
+    panic!("improper file format: {card}")
+}
+
+fn total_winnings() -> usize {
     // Get the sum of 2^wins for each card.
 
     if let Some(input) = fs::read_to_string("data/4.input").ok() {
-        return input.lines().filter_map(|line| {
-            if let Some((_, data)) = line.split_once(": ") {
-                if let Some((winning, ours)) = data.split_once(" | ") {
-                    let winning = winning
-                        .split_whitespace()
-                        .filter_map(|v| v.parse::<usize>().ok())
-                        .collect::<HashSet<usize>>();
+        return input.lines().map(|card| {
+            let wins = wins(card);
 
-                    let wins = ours
-                        .split_whitespace()
-                        .filter_map(|v| v.parse::<usize>().ok())
-                        .filter(|v| winning.contains(v))
-                        .collect::<Vec<usize>>()
-                        .len();
-
-                    // Danger arithmetic, casting here can’t be *too bad*.
-                    if wins == 0 {
-                        return None
-                    } else {
-                        return Some(2_i32.pow(wins as u32 - 1) as usize);
-                    }
-                }
+            if wins == 0 {
+                return 0;
+            } else {
+                return 1 << (wins - 1);
             }
+        }).sum();
+    }
 
-            None::<usize>
-        }).sum::<usize>();
+    panic!("file not found")
+}
+
+fn scratchcard_quantities() -> usize {
+    if let Some(input) = fs::read_to_string("data/4.input").ok() {
+        let lines = input.lines().collect::<Vec<&str>>();
+
+        // Winning n gives you an extra card for the next n cards.
+        // This compounds strongly so we need to keep adding extra cards consistently.
+        //
+        // We do this by tracking the quantities of each card, and adding to it as we go.
+        let mut quantities = vec![1; lines.len()];
+
+        lines.iter().enumerate().for_each(|(idx, line)| {
+            let wins = wins(line);
+
+            for i in idx+1..=idx+wins {
+                quantities[i] += quantities[idx];
+            }
+        });
+
+        return quantities.iter().sum();
     }
 
     panic!("file not found")
 }
 
 fn main() {
-    println!("part one: {}", winnings());
+    println!("part one: {}", total_winnings());
+    println!("part two: {}", scratchcard_quantities());
 }
