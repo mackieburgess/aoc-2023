deb71b6703f7c7afbf20f9bcae748f34e7c1738a
Mackie Burgess <mackie.burgess@gmail.com>
2023-12-13 07:50:37 +0000

    day 13 - part 2

--- src/day13.rs
+++ src/day13.rs
@@ -5,7 +5,27 @@ struct Mirror {
     vertical: Vec<Vec<bool>>
 }
 
-fn search_for_seam<T: PartialEq>(plane: &Vec<T>) -> Option<usize> {
+fn get_mirrors(mirrors: String) -> Vec<Mirror> {
+    mirrors.split("\n\n").map(|mirror| {
+        // Generate the horizontal mirror, alongside its transposition.
+
+        let horizontal = mirror.lines().map(|line| {
+            return line.chars().map(|c| c == '#').collect();
+        }).collect::<Vec<Vec<bool>>>();
+
+        let mut vertical = vec![];
+        for x in 0..horizontal[0].len() {
+            vertical.push(vec![]);
+            for y in 0..horizontal.len() {
+                vertical[x].push(horizontal[y][x]);
+            }
+        }
+
+        return Mirror { horizontal, vertical };
+    }).collect::<Vec<Mirror>>()
+}
+
+fn search_for_seam(plane: &Vec<Vec<bool>>) -> Option<usize> {
     // Find the first idx which can reduce into a reflection.
 
     for idx in 0..(plane.len() - 1) {
@@ -35,25 +55,51 @@ fn search_for_seam<T: PartialEq>(plane: &Vec<T>) -> Option<usize> {
     return None;
 }
 
-fn locate_mirror() -> usize {
-    if let Some(mirrors) = fs::read_to_string("data/13.input").ok() {
-        let mirrors = mirrors.split("\n\n").map(|mirror| {
-            // Generate the horizontal mirror, alongside its transposition.
+fn off_by_one(left: &Vec<bool>, right: &Vec<bool>) -> bool {
+    left.iter().zip(right).filter(|(l, r)| l != r).count() == 1
+}
 
-            let horizontal = mirror.lines().map(|line| {
-                return line.chars().map(|c| c == '#').collect();
-            }).collect::<Vec<Vec<bool>>>();
+fn search_for_smudge(plane: &Vec<Vec<bool>>) -> Option<usize> {
+    // Find the first idx which can reduce into a reflection, including a smudge.
+    // A smudge is an off-by-one error, where a single value is wrong.
 
-            let mut vertical = vec![];
-            for x in 0..horizontal[0].len() {
-                vertical.push(vec![]);
-                for y in 0..horizontal.len() {
-                    vertical[x].push(horizontal[y][x]);
+    for idx in 0..(plane.len() - 1) {
+        let mut left = idx;
+        let mut right = left + 1;
+        let mut found = false;
+        let mut smudge_used = false;
+
+        loop {
+            if plane[left] != plane[right] {
+                if !smudge_used && off_by_one(&plane[left], &plane[right]) {
+                    // Bypass the break if smudge hasn't been used.
+                    smudge_used = true;
+                } else {
+                    break;
                 }
             }
 
-            return Mirror { horizontal, vertical };
-        }).collect::<Vec<Mirror>>();
+            if left == 0 || right == plane.len() - 1 {
+                found = true;
+                break;
+            } else {
+                left -= 1;
+                right += 1;
+            }
+        }
+
+        // Ensure we used a smudge.
+        if found && smudge_used {
+            return Some(idx);
+        }
+    }
+
+    return None;
+}
+
+fn locate_mirrors() -> usize {
+    if let Some(mirrors) = fs::read_to_string("data/13.input").ok() {
+        let mirrors = get_mirrors(mirrors);
 
         return mirrors.iter().map(|mirror| {
             if let Some(seam) = search_for_seam(&mirror.horizontal) {
@@ -73,6 +119,32 @@ fn locate_mirror() -> usize {
     panic!("file not found")
 }
 
+fn locate_smudged_mirrors() -> usize {
+    // Instead of finding the reflective point of a mirror, find the first off-by-one error.
+
+    if let Some(mirrors) = fs::read_to_string("data/13.input").ok() {
+        let mirrors = get_mirrors(mirrors);
+
+        return mirrors.iter().map(|mirror| {
+            if let Some(seam) = search_for_smudge(&mirror.horizontal) {
+                // 100 * idx for horizontal mirrors.
+                return (seam + 1) * 100;
+            } else {
+                if let Some(seam) = search_for_smudge(&mirror.vertical) {
+                    // 1 * idx for vertical mirrors.
+                    return seam + 1;
+                }
+            }
+
+            return 0;
+        }).sum::<usize>()
+    }
+
+    panic!("file not found");
+
+}
+
 fn main() {
-    println!("part one: {}", locate_mirror());
+    println!("part one: {}", locate_mirrors());
+    println!("part two: {}", locate_smudged_mirrors());
 }
