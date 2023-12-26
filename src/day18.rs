41decf63a059bbf31bb592634a7d41d164535230
Mackie Burgess <mackie.burgess@gmail.com>
2023-12-26 15:21:33 +0000

    day 18 - part 2

--- src/day18.rs
+++ src/day18.rs
@@ -1,5 +1,14 @@
 use std::fs;
-use std::collections::HashMap;
+
+enum Mode {
+    Standard,
+    Hex
+}
+
+enum Turn {
+    Clockwise,
+    CounterClockwise
+}
 
 #[derive(Clone, Copy)]
 enum Direction {
@@ -9,28 +18,65 @@ enum Direction {
     Left
 }
 
+#[derive(Clone, Copy)]
 struct Instruction {
     // Hilarious to me that I’m storing these in a vector.
     direction: Direction,
-    magnitude: usize
+    magnitude: isize
 }
 
-fn read_instructions(instructions: String) -> Vec<Instruction> {
+impl Direction {
+    fn turn(&self, other: &Direction) -> Turn {
+        return match (self, other) {
+            (Direction::Up, Direction::Right) |
+            (Direction::Right, Direction::Down) |
+            (Direction::Down, Direction::Left) |
+            (Direction::Left, Direction::Up) => Turn::Clockwise,
+            (Direction::Up, Direction::Left) |
+            (Direction::Right, Direction::Up) |
+            (Direction::Down, Direction::Right) |
+            (Direction::Left, Direction::Down) => Turn::CounterClockwise,
+            _ => panic!()
+        }
+    }
+}
+
+fn read_instructions(instructions: String, mode: Mode) -> Vec<Instruction> {
     instructions
         .lines()
         .filter_map(|line| {
             if let Some((direction, rest)) = line.split_once(" ") {
-                if let Some((magnitude, _color)) = rest.split_once(" ") {
-                    let direction = match direction {
-                        "U" => Direction::Up,
-                        "R" => Direction::Right,
-                        "D" => Direction::Down,
-                        "L" => Direction::Left,
-                        _ => panic!("bad direction")
-                    };
+                if let Some((magnitude, color)) = rest.split_once(" ") {
+                    match mode {
+                        Mode::Standard => {
+                            let direction = match direction {
+                                "U" => Direction::Up,
+                                "R" => Direction::Right,
+                                "D" => Direction::Down,
+                                "L" => Direction::Left,
+                                _ => panic!("bad direction")
+                            };
 
-                    if let Some(magnitude) = magnitude.parse::<usize>().ok() {
-                        return Some(Instruction { direction, magnitude });
+                            if let Some(magnitude) = magnitude.parse::<isize>().ok() {
+                                return Some(Instruction { direction, magnitude });
+                            }
+                        },
+                        Mode::Hex => {
+                            let mut size_code = color.replace("(#", "").replace(")", "").to_uppercase();
+                            let dir_code = size_code.split_off(5);
+
+                            let direction = match dir_code.as_str() {
+                                "0" => Direction::Right,
+                                "1" => Direction::Down,
+                                "2" => Direction::Left,
+                                "3" => Direction::Up,
+                                _ => unreachable!()
+                            };
+
+                            if let Some(magnitude) = isize::from_str_radix(&size_code, 16).ok() {
+                                return Some(Instruction { direction, magnitude });
+                            }
+                        }
                     }
                 }
             }
@@ -39,106 +85,57 @@ fn read_instructions(instructions: String) -> Vec<Instruction> {
         }).collect()
 }
 
-fn lava_bowl_area() -> usize {
-    // Use a turtle to draw a lava bowl, then calculate the flood fill of the
-    // shape it makes.
-
+fn lava_bowl_area(mode: Mode) -> isize {
     if let Some(instructions) = fs::read_to_string("data/18.input").ok() {
-        let instructions = read_instructions(instructions);
+        let mut instructions = read_instructions(instructions, mode);
 
-        let mut cursor = (0, 0);
+        // Copy the first movement over to the end, so that we can calculate the corners.
+        instructions.push(instructions[0].clone());
+        let instructions = instructions;
 
-        let mut found = HashMap::new();
+        let mut cursor: (isize, isize) = (0, 0);
+        let mut points = vec![cursor];
 
-        for (idx, instruction) in instructions.iter().enumerate() {
-            let movement = match instruction.direction {
-                Direction::Up   | Direction::Down  => '|',
-                Direction::Left | Direction::Right => '-'
+        // The last direction, accounting for the repeat of the first element.
+        let mut initial_direction = instructions[instructions.len() - 2].direction;
+
+        for instruction_pair in instructions.windows(2) {
+
+            // Increment handles drawing a line around the lava bowl wall, instead of “on it”.
+            let increment: isize = match (
+                initial_direction.turn(&instruction_pair[0].direction),
+                instruction_pair[0].direction.turn(&instruction_pair[1].direction)
+            ) {
+                (Turn::Clockwise, Turn::Clockwise) => 1,
+                (Turn::CounterClockwise, Turn::CounterClockwise) => -1,
+                _ => 0
             };
 
-            for _ in 0..(instruction.magnitude - 1) {
-                match instruction.direction {
-                    Direction::Up    => cursor.1 -= 1,
-                    Direction::Right => cursor.0 += 1,
-                    Direction::Down  => cursor.1 += 1,
-                    Direction::Left  => cursor.0 -= 1
-                }
-
-                found.insert(cursor, movement);
-            }
-
-            // Handle the turning.
-
-            match instruction.direction {
-                Direction::Up    => cursor.1 -= 1,
-                Direction::Right => cursor.0 += 1,
-                Direction::Down  => cursor.1 += 1,
-                Direction::Left  => cursor.0 -= 1
-            }
-
-            let next_direction = match idx < instructions.len() - 1 {
-                true => instructions[idx + 1].direction,
-                false => instructions[0].direction
+            match instruction_pair[0].direction {
+                Direction::Up    => cursor.1 -= instruction_pair[0].magnitude + increment,
+                Direction::Right => cursor.0 += instruction_pair[0].magnitude + increment,
+                Direction::Down  => cursor.1 += instruction_pair[0].magnitude + increment,
+                Direction::Left  => cursor.0 -= instruction_pair[0].magnitude + increment
             };
 
-            // Parse corners into our day 10 solution.
-            let final_movement = match (instruction.direction, next_direction) {
-                (Direction::Up, Direction::Left)    | (Direction::Right, Direction::Down) => '7',
-                (Direction::Up, Direction::Right)   | (Direction::Left, Direction::Down)  => 'F',
-                (Direction::Down, Direction::Left)  | (Direction::Right, Direction::Up)   => 'J',
-                (Direction::Down, Direction::Right) | (Direction::Left, Direction::Up)    => 'L',
-                _ => unreachable!()
-            };
+            points.push(cursor);
 
-            found.insert(cursor, final_movement);
+            initial_direction = instruction_pair[0].direction;
         }
 
-        let (min_x, min_y) = (
-            found.keys().map(|f| f.0).min().unwrap(),
-            found.keys().map(|f| f.1).min().unwrap(),
-        );
+        // Shoelace formula: a maths shorthand for calculating the area of a polygon.
+        points
+            .windows(2)
+            .fold(0, |acc, pair| {
+                return acc + (pair[0].0 * pair[1].1) - (pair[0].1 * pair[1].0);
+            }) / 2
 
-        let (max_x, max_y) = (
-            found.keys().map(|f| f.0).max().unwrap(),
-            found.keys().map(|f| f.1).max().unwrap(),
-        );
-
-        let mut fill = 0;
-
-        // Reuses my day 10 solution for counting enclosed tiles.
-        for y in min_y..=max_y {
-            let mut enclosed = false;
-            let mut unflip = '.';
-
-            for x in min_x..=max_x {
-                if let Some(wall_piece) = found.get(&(x, y)) {
-                    match wall_piece {
-                        '|' => enclosed = !enclosed,
-                        'F' => {
-                            enclosed = !enclosed;
-                            unflip = '7';
-                        },
-                        'L' => {
-                            enclosed = !enclosed;
-                            unflip = 'J';
-                        },
-                        v if *v == unflip => {
-                            enclosed = !enclosed;
-                        },
-                        _ => {}
-                    }
-                } else {
-                    if enclosed { fill += 1 }
-                }
-            }
-        }
-
-        return found.len() + fill;
+    } else {
+        panic!("file not found")
     }
-
-    panic!("file not found")
 }
 
 fn main() {
-    println!("part one: {}", lava_bowl_area());
+    println!("part one: {}", lava_bowl_area(Mode::Standard));
+    println!("part two: {}", lava_bowl_area(Mode::Hex));
 }
